9fa44095c437812cc63b846dc567c1143262b06a
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2023-03-02 17:48:25 +0530

    Update Basic.cpp

--- Linked_LIST/Basic.cpp
+++ Linked_LIST/Basic.cpp
@@ -21,3 +21,37 @@ void push(struct Node** ref , int val)
 
 }
 
+
+
+
+
+
+
+
+
+
+struct Node* newNode(int key)
+{
+    struct Node* temp
+        = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = key;
+    temp->next = NULL;
+    return temp;
+}
+ 
+// Driver Code
+int main()
+{
+    struct Node* head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(3);
+    head->next->next->next = newNode(4);
+    head->next->next->next->next = newNode(5);
+ 
+    /* Create a loop for testing */
+    head->next->next->next->next->next = head->next;
+ 
+    cout << countNodesinLoop(head) << endl;
+ 
+    return 0;
+}
